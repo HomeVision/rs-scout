a0790fa8e6dad009fbb13279bd23049d3495ab04
Vincent Chu <vincentchu@gmail.com>
2023-05-20 06:17:29 -0700

    convert root path

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,25 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::time::SystemTime;
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn root() -> impl Responder {
+    HttpResponse::Ok().body(format!(
+        "<html>
+            <body>
+                <h1>Scout, <em>at your service</em></h1>
+                <p>{:?}</p>
+            </body>
+        </html>",
+        SystemTime::now()
+    ))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(root))
+        .bind(("0.0.0.0", 8000))?
+        .run()
+        .await
 }
 
 // #[macro_use]
