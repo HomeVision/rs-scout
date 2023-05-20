1ee5737660883c84fcdfe87dcb1aded6950a47ff
Vincent Chu <vincentchu@gmail.com>
2023-05-20 05:58:05 -0700

    Switch deploy to use rsync

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
 DEST=scout:/root/workspace/rs-scout
-FILES=(Cargo.toml Cargo.lock Rocket.toml container.sh Dockerfile.scout src/)
+FILES="Cargo.toml Cargo.lock Rocket.toml container.sh Dockerfile.scout src"
 
-function copy_to_host() {
-  echo $@
-  scp -F ~/.ssh/config -r $@ $DEST
-}
+rsync -avzr --progress $FILES $DEST
 
-for FILE in "${FILES[@]}"
-do
-  echo "Copying $FILE => $DEST"
-  copy_to_host $FILE
-done
 
 
