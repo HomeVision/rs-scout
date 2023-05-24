21658d8554fab2333a37d290e8170fe55fb3a32b
Vincent Chu <vincentchu@gmail.com>
2023-05-24 14:56:49 -0700

    Resolve sorting bug (#1)
    This fixes a bug where the returned `SearchResults` were correct, but
    unsorted. To do this, I had to implement `PartialEq`, `Eq`, `PartialOrd`
    and `Ord` on the `SearchResult` and call `sort_by` with a reverse order
    on the results Vec

--- src/vector_index.rs
+++ src/vector_index.rs
@@ -1,6 +1,7 @@
 use crate::sent_transform;
 use sbert::{self, Embeddings};
 use serde::{Deserialize, Serialize};
+use std::cmp::{Eq, Ord, Ordering, PartialEq, PartialOrd};
 use std::sync;
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
@@ -9,11 +10,31 @@ pub struct TextBody {
     pub text: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialOrd)]
 pub struct SearchResult {
     pub id: String,
     pub text: String,
     pub score: f32,
+}
+
+impl PartialEq for SearchResult {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id && self.text == other.text && self.score == other.score
+    }
+}
+
+impl Eq for SearchResult {}
+
+impl Ord for SearchResult {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.score < other.score {
+            Ordering::Less
+        } else if self.score == other.score {
+            Ordering::Equal
+        } else {
+            Ordering::Greater
+        }
+    }
 }
 
 struct Index {
@@ -112,7 +133,7 @@ impl GuardedIndex {
             .map_err(|_| String::from("search_knn: Failed to acquire lock"))
             .and_then(|idx| {
                 sent_transform::search_knn(query, &idx.embeddings, results).map(|raw_results| {
-                    raw_results
+                    let mut results: Vec<SearchResult> = raw_results
                         .iter()
                         .map(|raw_result| {
                             let text_body = &idx.texts[raw_result.index];
@@ -123,7 +144,11 @@ impl GuardedIndex {
                                 score: raw_result.score,
                             }
                         })
-                        .collect()
+                        .collect();
+
+                    results.sort_by(|x, y| y.cmp(&x)); // Sort Vector in descending order
+
+                    results
                 })
             })
     }
