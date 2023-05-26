cc3b9f9895969b27f1e4f2cdc18c1ed5a15cadf1
Vincent Chu <vincentchu@gmail.com>
2023-05-25 22:20:34 -0700

    Clean up and remove unwraps to make this thing a bit better

--- src/sent_transform/svm.rs
+++ src/sent_transform/svm.rs
@@ -13,27 +13,23 @@ pub fn svm(q: &Vec<f32>, vectors: &[Vec<f32>]) -> Result<Vec<f64>, String> {
     let mut labels = vec![0.0; nvecs + 1];
     labels[0] = 1.0;
 
-    let mut features: Vec<Vec<(u32, f64)>> = vectors.iter().map(vec_to_features).collect();
-    let features2 = features.clone();
-    features.insert(0, vec_to_features(q));
+    let mut all_embeddings: Vec<Vec<(u32, f64)>> = vectors.iter().map(vec_to_features).collect();
+    let features2 = all_embeddings.clone();
+    all_embeddings.insert(0, vec_to_features(q));
 
     let mut model_builder = liblinear::Builder::new();
 
     model_builder
         .problem()
-        .input_data(util::TrainingInput::from_sparse_features(labels, features).unwrap())
+        .input_data(util::TrainingInput::from_sparse_features(labels, all_embeddings).unwrap())
         .bias(1f64);
 
     let n_samples = (nvecs + 1) as f64;
     let weights = vec![n_samples / (2.0 * (nvecs as f64)), n_samples / 2.0];
-    println!("WEIGHTS = {:?}", weights);
 
     model_builder
         .parameters()
-        .solver_type(SolverType::L2R_L2LOSS_SVC_DUAL) // iter=7, -2.87500, nSV=3
-        // .solver_type(SolverType::L2R_L1LOSS_SVC_DUAL) // iter=2 -2.9500, nSV=3
-        // .solver_type(SolverType::MCSVM_CS) // iter=2 -2.9000, nSV=6
-        // .solver_type(SolverType::L2R_LR_DUAL) // iter=10 -0.206
+        .solver_type(SolverType::L2R_L2LOSS_SVC_DUAL)
         .stopping_criterion(1e-6)
         .constraints_violation_cost(0.1)
         .cost_penalty_labels(vec![0, 1])
@@ -43,24 +39,19 @@ pub fn svm(q: &Vec<f32>, vectors: &[Vec<f32>]) -> Result<Vec<f64>, String> {
         .build_model()
         .map_err(|err| format!("svm: Error creating model: {err}"))?;
 
-    let distances = features2
+    features2
         .iter()
         .enumerate()
         .map(|(idx, feature)| {
-            let intput = util::PredictionInput::from_sparse_features(feature.to_vec())
+            util::PredictionInput::from_sparse_features(feature.to_vec())
                 .map_err(|err| format!("svm: Failed to create prediction input {idx}: {err}"))
-                .unwrap();
-
-            let (dists, _) = model
-                .predict_values(intput)
-                .map_err(|err| format!("svm: Failed to predict values input {idx}: {err}"))
-                .unwrap();
-
-            println!("RESULTS {idx}: {:?}", dists);
-
-            dists.first().map_or(-100.0, |f| *f)
+                .and_then(|input| {
+                    model
+                        .predict_values(input)
+                        .map_err(|err| format!("svm: Failed to predict values input {idx}: {err}"))
+                        .map(|(dists, _)| dists.first().map_or(-100.0, |f| *f))
+                })
         })
-        .collect();
-
-    Ok(distances)
+        .into_iter()
+        .collect()
 }
